eb0a91efc077340d20be345bb0bb0b6cbaf075eb
Cezar Eduardo Ceolatto <cezareducg@gmail.com>
2023-05-16 14:59:36 -0300

    resolve exercicio1

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,25 @@
+// 1 - Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+// *Dica: Neste link você encontra mais detalhes sobre operadores matemáticos. 😉
+
+// De olho na dica 👀: Use o console.log() para exibir o que cada exercício pede.
+
+let a = 10;
+let b = 10;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log('a soma é' , adicao);
+console.log('a subtração é' , subtracao);
+console.log('a multiplicação é', multiplicacao);
+console.log('a divisão é' , divisao);
+console.log('em módulo' , modulo);
