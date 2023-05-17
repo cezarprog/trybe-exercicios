aacae394e8bfd5896a4ca7b1a25487968c7931b7
Cezar Eduardo Ceolatto <cezareducg@gmail.com>
2023-05-17 17:15:53 -0300

    exercicio 7

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio8.js
@@ -0,0 +1,3 @@
+// 08 - Utilizando for, crie um array que vá de 1 a 25 e imprima o resultado.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio9.js
@@ -0,0 +1,3 @@
+// 09 - Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
