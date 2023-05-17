2ca5c9f61624283f09acd3353685c4786fb1c9f8
Cezar Eduardo Ceolatto <cezareducg@gmail.com>
2023-05-17 17:21:53 -0300

    exercicio 9

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio9.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio9.js
@@ -1,3 +1,12 @@
 // 09 - Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [
+    1,  2,  3,  4,  5,  6,  7,  8,
+    9, 10, 11, 12, 13, 14, 15, 16,
+   17, 18, 19, 20, 21, 22, 23, 24,
+   25
+]
+
+for (let index = 0; index < numbers.length; index +=1) {
+    console.log(numbers[index] / 2);
+}
